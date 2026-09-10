c05682675b45728e1922470dc52a9ac883b33654
ize12 <b2nagkor@vasvari.org>
2026-09-10 10:36:44 +0200

    typeok tanulasa

--- app.ts
+++
@@ -1,3 +0,0 @@
-const x: number = 5;
-
-const nevek: Array<string> = ["a", "b", "c"];
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+const x: number = 5;
+
+const nevek: Array<string> = ["a", "b", "c"];
+console.log(nevek);
+// typeok
+type Ember = {
+  id?: number;
+  nev: string;
+  kor: number;
+};
+
+const valaki: Ember = { nev: "Pisti", kor: 25 };
+
+console.log(valaki);
+const emberek: Array<Ember> = [
+  { nev: "Pisti", kor: 25 },
+  { nev: "Jani", kor: 37 },
+  { nev: "Lilla", kor: 16 },
+];
+
+const felnottek: Array<Ember> = emberek.filter((i: Ember) => i.kor >= 18);
+console.log(felnottek);
